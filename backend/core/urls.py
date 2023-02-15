542cf63d59d9995b2af69ffed7d766dbdc461f87
Diego Masin <diegoifce@gmail.com>
2023-02-14 22:03:23 -0300

    Adjusting a minor details to run migrate.

---
+++ backend/core/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+app_name = 'core'
+
+urlpatterns = [
+    # part('', v.index, name='index'), # noqa E501
+]
--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-from decouple import config
+from decouple import config, Csv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
